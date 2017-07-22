c10ebab2c53d6a39f3d28a952a56c1b7a301599b
Oleg Timofeev <oleg.timofeev20@gmail.com>
2017-07-22 15:29:36 +0300

    null

---
+++ RAMCard.Domain/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RAMCard.Domain
+{
+    public class BaseEntity
+    {
+        public string Id { get; set; }
+
+        public DateTime TimeCreated { get; set; }
+
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedTime { get; set; }
+    }
+}
---
+++ RAMCard.Domain/User/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace RAMCard.Domain.User
+{
+    public class AppUser : BaseEntity
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public string Password { get; set; }
+    }
+}
